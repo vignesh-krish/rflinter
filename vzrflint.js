daea4eeedcbea5cd90e6916995f16e74f432550b
vignesh-krish <vigneshsk.jmc@gmail.com>
2016-11-14 00:45:15 +0530

    clean up - removed unused functions

--- vzrflint.js
+++ vzrflint.js
@@ -1,9 +1,7 @@
 //lint js
 (function(exports){
 	
-	 var NUM_COLS = 12;
-    var COL_REGEX = /\bcol-(xs|sm|md|lg)-(\d{1,2})\b/;
-    var COL_REGEX_G = /\bcol-(xs|sm|md|lg)-(\d{1,2})\b/g;
+	 var NUM_COLS = 12;   
     var COL_CLASSES = [];
     var SCREENS = ['tiny', 'small', 'medium', 'large'];
     SCREENS.forEach(function (screen) {
@@ -11,98 +9,17 @@
             COL_CLASSES.push( '.'+screen + '-' + n);
         }
     });
-    var SCREEN2NUM = {
-        'xs': 0,
-        'sm': 1,
-        'md': 2,
-        'lg': 3
-    };
-    var NUM2SCREEN = ['xs', 'sm', 'md', 'lg'];
-   var IN_NODE_JS = {};
-    function compareNums(a, b) {
-        return a - b;
-    }
-
-    function isDoctype(node) {
-        return node.type === 'directive' && node.name === '!doctype';
-    }
-
    
-
-    function withoutClass(classes, klass) {
-        return classes.replace(new RegExp('\\b' + klass + '\\b', 'g'), '');
-    }
-
-    function columnClassKey(colClass) {
-        return SCREEN2NUM[COL_REGEX.exec(colClass)[1]];
-    }
-
-    function compareColumnClasses(a, b) {
-        return columnClassKey(a) - columnClassKey(b);
-    }
-
-    /**
-     * Moves any grid column classes to the end of the class string and sorts the grid classes by ascending screen size.
-     * @param {string} classes The "class" attribute of a DOM node
-     * @returns {string} The processed "class" attribute value
-     */
-    function sortedColumnClasses(classes) {
-        // extract column classes
-        var colClasses = [];
-        while (true) {
-            var match = COL_REGEX.exec(classes);
-            if (!match) {
-                break;
-            }
-            var colClass = match[0];
-            colClasses.push(colClass);
-            classes = withoutClass(classes, colClass);
-        }
-
-        colClasses.sort(compareColumnClasses);
-        return classes + ' ' + colClasses.join(' ');
-    }
-
-    /**
-     * @param {string} classes The "class" attribute of a DOM node
-     * @returns {Object.<string, integer[]>} Object mapping grid column widths (1 thru 12) to sorted arrays of screen size numbers (see SCREEN2NUM)
-     *      Widths not used in the classes will not have an entry in the object.
-     */
-    function width2screensFor(classes) {
-        var width = null;
-        var width2screens = {};
-        while (true) {
-            var match = COL_REGEX_G.exec(classes);
-            if (!match) {
-                break;
-            }
-            var screen = match[1];
-            width = match[2];
-            var screens = width2screens[width];
-            if (!screens) {
-                screens = width2screens[width] = [];
-            }
-            screens.push(SCREEN2NUM[screen]);
-        }
-
-        for (width in width2screens) {
-            if (width2screens.hasOwnProperty(width)) {
-                width2screens[width].sort(compareNums);
-            }
-        }
-
-        return width2screens;
-    }
      function LintError(id, message, elements) {
         this.id = id;
         this.message = message;
-        this.elements = elements || cheerio('');
+        this.elements = elements;
     }
     exports.LintError = LintError;
     var allLinters = {};
     function addLinter(id, linter) {
         if (allLinters[id]) {
-            /* @covignore */
+            
             throw new Error("Linter already registered with ID: " + id);
         }
 
@@ -114,7 +31,7 @@
             Problem = LintWarning;
         }
         else {
-            /* @covignore */
+          
             throw new Error("Invalid linter ID: " + id);
         }
 
@@ -122,7 +39,7 @@
             function specializedReporter(message, elements) {
                 reporter(new Problem(id, message, elements));
             }
-            	console.log(linter)
+            	
             linter($, specializedReporter);
         }
 
@@ -136,7 +53,7 @@
         }).join(',');
 
         var colsOutsideRowsAndFormGroups = $(selector);       
-       // reporter("Columns (`.col-*-*`) can only be children of `.row`s or `.form-group`s", colsOutsideRowsAndFormGroups);
+      
         if (colsOutsideRowsAndFormGroups.length) {
             reporter("Columns (`.columns *-*`) can only be children of `.row`s", colsOutsideRowsAndFormGroups);
         }
@@ -161,7 +78,7 @@
         });
         Object.keys(allLinters).sort().forEach(function (linterId) {
             if (!disabledIdSet[linterId]) {
-                console.log(linterId);
+               // console.log(linterId);
                 allLinters[linterId]($, reporterWrapper);
 
             }
@@ -171,7 +88,7 @@
             var $ = (jQuery);
          
             exports.lintCurrentDocument = function (reporter, disabledIds) {
-            	console.log("lintCurrentDocument")
+            	//console.log("lintCurrentDocument")
                 this._lint($, reporter, disabledIds);
             };
             
@@ -184,13 +101,13 @@
                 var errorCount = 0;
 
                 var reporter = function (lint) {
-                	console.log("reporter")
+                	
                     var background = "background: #" + (lint.id[0] === "W" ? "f0ad4e" : "d9534f") + "; color: #ffffff;";
                     if (!seenLint) {
                         if (alertOnFirstProblem) {
-                            /*eslint-disable no-alert, no-undef, block-scoped-var */
-                            window.alert("vzrfLint found errors in this document! See the JavaScript console for details.");// jshint ignore:line
-                            /*eslint-enable no-alert, no-undef, block-scoped-var */
+                           
+                            window.alert("vzrfLint found errors in this document! See the JavaScript console for details.");
+                            
                         }
                         seenLint = true;
                     }
@@ -209,14 +126,13 @@
                     console.info("vzrfLint: For details, look up the lint problem IDs in the html");
                 }
                 else if (alertIfNoProblems) {
-                    /*eslint-disable no-alert, no-undef, block-scoped-var */
+                   
                     window.alert("vzrfLint found no errors in this document.");// jshint ignore:line
-                    /*eslint-enable no-alert, no-undef, block-scoped-var */
+                    
                 }
             };
-            /*eslint-disable no-undef, block-scoped-var */
-            window.vzrfLint = exports;// jshint ignore:line
-            /*eslint-enable no-undef, block-scoped-var */
+           
+            window.vzrfLint = exports;
         })();
 
 })(typeof exports === 'object' && exports || this);
